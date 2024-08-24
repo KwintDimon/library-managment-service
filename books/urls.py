a8b3ee206c8cf4ae5e5f1b2f84f89205cbc2903f
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-08-24 23:05:05 +0300

    connected to PostgreSQL and dockerized with .env file

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from books.views import BookListCreateView, BookDetailView
 
+app_name = "books"
+
 urlpatterns = [
     path("", BookListCreateView.as_view(), name="book-list-create"),
     path("<int:pk>/", BookDetailView.as_view(), name="book-detail"),
 ]
-
-app_name = "books"
--- library_service/settings.py
+++ library_service/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i6-@_+!(82ccgw75$z=mg+fsx((qm@1_b4pqpo2tmzb&9mq!q-"
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -80,9 +85,13 @@ WSGI_APPLICATION = "library_service.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
 
